0c5a64f39d4672bef74fb6a04683924a2e9e0bc3
Rochelle Lee <rochelle.y.lee@gmail.com>
2017-08-19 18:44:35 -0700

    starting to add content to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+    /*const actionButtons = ["PARTICIPATE", "FEEDBACK", "DONATE"].map((action) =>
+      {
+        return (
+          <button actionButton
+        );
+      });*/
+
     return (
       <div className="footer">
         <h2>FOOTER</h2>
