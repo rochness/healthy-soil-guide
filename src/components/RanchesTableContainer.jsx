78a74b5304cd624cf2d402729a129d6a2a6df673
Rochelle Lee <rochelle.y.lee@gmail.com>
2017-08-19 18:45:46 -0700

    updated dummy ranches data

--- src/components/RanchesTableContainer.jsx
+++ src/components/RanchesTableContainer.jsx
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import Reactable from "reactable";
 import FarmsAndRanchesTable from "./FarmsAndRanchesTable";
+import { ranchData } from "../data/data.js";
 
 class RanchesTableContainer extends Component {
   constructor(props) {
     super(props);
-    const name = "Ranch";
-    const zipCode = 90000;
-    const comparedToRegion = 5.5;
-    const improvement = 2.2;
-    const bestPractice = "blah";
     this.state = {
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => {
-      return {
-        name: name.concat(num),
-        zipCode: zipCode + num,
-        comparedToRegion: (comparedToRegion + num),
-        improvement: (improvement - num),
-        bestPractice: bestPractice.concat(num),
-        };
-      }),
+      data: ranchData,
     };
   }
 
--- src/data/data.js
+++ src/data/data.js
@@ -69,3 +69,75 @@ export const farmData = [
     },
   },
 ];
+
+export const ranchData = [
+  {
+    name: "Deep Roots Ranch",
+    zipCode: "Greenbrae, CA 94904",
+    comparedToRegion: 5.0,
+    improvement: 0.3,
+    bestPractice: {
+      noTill: true,
+      cropRotation: true,
+      composting: true,
+    },
+  },
+  {
+    name: "True Seed Ranch",
+    zipCode: "Brentwood, CA 94914",
+    comparedToRegion: 4.4,
+    improvement: 2.9,
+    bestPractice: {
+      noTill: true,
+      coverCrop: true,
+      cropRotation: true,
+      composting: true,
+      hedgerows: true,
+    },
+  },
+  {
+    name: "Grassy Grass Ranch",
+    zipCode: "Marin, CA 94924",
+    comparedToRegion: 5.6,
+    improvement: 1.4,
+    bestPractice: {
+      noTill: true,
+      coverCrop: true,
+      cropRotation: true,
+      composting: true,
+      hedgerows: true,
+    },
+  },
+  {
+    name: "Hidden Valley Ranch",
+    zipCode: "Bakersfield, CA 90204",
+    comparedToRegion: 1.6,
+    improvement: -1.5,
+    bestPractice: {
+      cropRotation: true,
+    },
+  },
+  {
+    name: "Westgrass Beef",
+    zipCode: "Tomales Bay, CA 94914",
+    comparedToRegion: 4.2,
+    improvement: 0.5,
+    bestPractice: {
+      noTill: true,
+      cropRotation: true,
+      composting: true,
+    },
+  },
+  {
+    name: "Rancho Cucamongo",
+    zipCode: "Salinas, CA 93906",
+    comparedToRegion: 3.8,
+    improvement: 2.1,
+    bestPractice: {
+      noTill: true,
+      coverCrop: true,
+      cropRotation: true,
+      hedgerows: true,
+    },
+  },
+];
