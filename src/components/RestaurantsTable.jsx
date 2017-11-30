b728dfd1a24175b42521fbf333b48fd4c9158b20
Rochelle Lee <rochelle@meraki.com>
2017-11-30 01:07:43 -0800

    using phony restaurant logo data

--- src/components/RestaurantsTable.jsx
+++ src/components/RestaurantsTable.jsx
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import Reactable from "reactable";
+import restaurantData from "../data/restaurants.js";
+
+const LogoItem = (props) => {
+  return (
+    <div className="logo-container">
+      <a href={ props.website_url }>
+        <img className="restaurant-logo"
+          src={ props.logo_url }
+        />
+      </a>
+    </div>
+  );
+};
 
 class RestaurantsTable extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      restaurants: restaurantData,
+    };
+  }
+
+  /*componentWillMount() {
+    let restaurants = [];
+    let dataQuery = fire.database().ref("restaurants").orderByKey();
+     dataQuery.once('value')
+      .then(snapshot => {
+        snapshot.forEach(childSnapshot => {
+          data.push(childSnapshot.val());
+        });
+        this.setState({restaurants: restaurants});
+      });
+  }*/
+
+  renderLogoRow() {
+    let numberOfRows = Math.ceil(this.state.restaurants.length / 3);
+    let rows = [];
+    for (let i=0; i < numberOfRows; i++) {
+      let startingIndex = 3 * i;
+      let endingIndex = startingIndex + 3;
+      let subset = this.state.restaurants.slice(startingIndex, endingIndex);
+      let logos = subset.map((restaurant) => <LogoItem {...restaurant }/>);
+      rows.push((
+        <div className="restaurant-logo-row">
+          { logos }
+        </div>
+      ));
+    }
+    return rows;
   }
 
   render() {
     return (
-      <div className="tableContainer twelve columns">
-        <div className="placeholder">
-          <p> Coming soon... </p>
-        </div>
+      <div className="tableContainer restaurant-container">
+        { this.renderLogoRow() }
       </div>
     );
   }
---
+++ src/data/restaurants.js
@@ -0,0 +1,28 @@
+const restaurants = [
+  {
+    website_url: "www.aaa.com",
+    logo_url: "/assets/brown_sugar_kitchen.jpg",
+  },
+  {
+    website_url: "www.aaa.com",
+    logo_url: "/assets/benu.jpg",
+  },
+  {
+    website_url: "www.aaa.com",
+    logo_url: "/assets/atelier_crenn.jpg",
+  },
+  {
+    website_url: "www.aaa.com",
+    logo_url: "/assets/Blue-Hill-NYC.jpg",
+  },
+  {
+    website_url: "www.aaa.com",
+    logo_url: "/assets/commonwealth.jpg",
+  },
+  {
+    website_url: "www.aaa.com",
+    logo_url: "/assets/arguello_logo.jpg",
+  },
+];
+
+export default restaurants;
